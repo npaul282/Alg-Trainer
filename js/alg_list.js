80861ab585276742e62d74c54dec1fb9a98b75d3
npaul282 <npaul282@gmail.com>
2021-01-12 22:25:19 +1000

    Update alg_list.js

--- js/alg_list.js
+++ js/alg_list.js
@@ -2,7 +2,7 @@
 var zbll_egg = {
 
     "T1": ["(U') R U R' U R U2 R' U2 R' U' R U' R' U2 R", "x D' R' U R D R2' D2 R U' R' D2 R2 x'", "R U2 R' U' R U' R2 U2 R U R' U R", "(U2) R' U2 R U R' U R2 U2 R' U' R U' R'", "(U') R U R' U R U2 R' U' R U2 R' U' R U' R'", "(U2) R U' R' U2 R U R' U2 R U R' U R U' R'", "R U2 R' U' R U' R' U R U R' U R U2 R'", "(U2) R' U2 R U R' U R U' R' U' R U' R' U2 R", "(U') R U R' U R U' R' U R' U' R2 U' R2 U2 R", "(U') R' U' R U' R' U R U' R U R2 U R2 U2 R'", "R U' R2 F R F' R U' B U' B' U' R'", "(U') R' U' R2 U R2 U R2 U2 R' U R' U R"],
-    "T2": [""],
+    "T2": ["(U2) R U' R2 D' r U2 r' D R2 U R'","R' U R2 D r' U2 r D' R2 U' R","(U) R U R' U R U R2 D' r U2 r' D R2 U R'", "(U) R U' R2 D' r U2 r' D R2 U' R' U' R U' R'", "(U2) R U R' U2 R' D' R U R' D R2 U' R' U R U' R'", "(U) F U' R U R2 D' r U2 r' D R U' F'", "(U) R U R' U' R U R2 D' R U' R' D R U2 R U' R'", "F U R' D' r U2 r' D R2 U' R' U F'", "(U2) r U2 R2 F R F' R U2 B2 r' U' r B' r'", "(U') F' U' R' F R S' R U' B R' F' R B' U R' f", "R' U D' R U2 R' D R' U' R U2 R' U' R2", "(U2) R D U' R' U2 R D' R U R' U2 R U R2"],
     "T3": [""],
     "T4": [""],
     "T5": [""],
